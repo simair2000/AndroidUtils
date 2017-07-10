f207107e4c9a7a94d858117ca74dcf1d213e27c5
sanghoon.kim <sanghoon.kim@tisquare.com>
2017-07-10 15:49:47 +0900

    Endless Listener 추가

---
+++ 00.AndroidUtils/app/src/main/java/com/simair/android/androidutils/ui/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,79 @@
+package com.simair.android.androidutils.ui;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by simair on 17. 7. 10.
+ * Referenced at <br />
+ * @see <a href="https://gist.github.com/ssinss/e06f12ef66c51252563e">https://gist.github.com/ssinss/e06f12ef66c51252563e</a>
+ */
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+    public static String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
+
+    private int previousTotal = 0; // The total number of items in the dataset after the last load
+    private boolean loading = true; // True if we are still waiting for the last set of data to load.
+    private int visibleThreshold = 5; // The minimum amount of items to have below your current scroll position before loading more.
+    int firstVisibleItem, visibleItemCount, totalItemCount;
+
+    private int current_page = 1;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    boolean loadingTop;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+    public void reset() {
+        previousTotal = 0;
+        loading = true;
+        firstVisibleItem = 0;
+        visibleItemCount = 0;
+        totalItemCount = 0;
+        current_page = 1;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mLinearLayoutManager.getItemCount();
+        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (loading) {
+            if (totalItemCount > previousTotal) {
+                loading = false;
+                previousTotal = totalItemCount;
+            }
+        }
+        if(!loading) {
+            if ((totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                // End has been reached
+
+                // Do something
+                current_page++;
+
+                onLoadMore(current_page);
+
+                loading = true;
+            }
+        }
+
+        if(firstVisibleItem == 0 && dy < 0 && !loadingTop) {
+            onLoadTop();
+            loadingTop = true;
+        }
+    }
+
+    public void refreshTopComplete() {
+        loadingTop = false;
+    }
+
+    public abstract void onLoadMore(int current_page);
+
+    public void onLoadTop() {}
+}
